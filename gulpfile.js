d070d7065fa049f984a9ec51179521165fc2ef38
Vladimir.Zhidal <vzhidal@mercle.com>
2015-05-24 18:39:40 +0300

    Completed a draft version.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var watchLess = require('gulp-watch-less');
 var less = require('gulp-less');
-
+var csscomb = require('gulp-csscomb');
 
 
 gulp.task('default', function () {
@@ -9,4 +9,12 @@ gulp.task('default', function () {
 		.pipe(watchLess('styles/**/*.less'))
 		.pipe(less())
 		.pipe(gulp.dest('dist'));
+});
+
+
+
+gulp.task('csscomb', function () {
+	return gulp.src('styles/**/*.css')
+		.pipe(csscomb())
+		.pipe(gulp.dest('styles'));
 });
